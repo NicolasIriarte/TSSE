a6c35ba37a9300a5ba3dcaad3fd950bd93112e1a
Nicolás Iriarte <NicolasIriarte95@gmail.com>
2024-03-21 20:54:07 -0300

    Add Tp5 and pre-commit all files

--- TP2/src/leds.c
+++ TP2/src/leds.c
@@ -1,6 +1,16 @@
 #include "leds.h"
 #include <stddef.h>
 
+#include <stdio.h>
+
+#define RETURN_IF_INVALID_ADDRESS(ADDR)                                        \
+  if (ADDR == NULL)                                                            \
+    return -1;
+
+#define RETURN_IF_INVALID_RANGE(LED_ID)                                        \
+  if (LED_ID < 1 || LED_ID > 16)                                               \
+    return -1;
+
 static uint16_t *leds_ = NULL;
 static uint16_t state_;
 
@@ -9,8 +19,8 @@ enum { ALL_LEDS_OFF = 0, ALL_LEDS_ON = ~ALL_LEDS_OFF };
 static int NumberToBit(int led_idx) { return 1 << (led_idx - 1); }
 
 int Leds_Init(uint16_t *address) {
-  if (address == NULL)
-    return -1;
+  RETURN_IF_INVALID_ADDRESS(address)
+
   leds_ = address;
   state_ = ALL_LEDS_OFF;
   *leds_ = state_;
@@ -18,12 +28,9 @@ int Leds_Init(uint16_t *address) {
 };
 
 int Leds_TurnOn(int led) {
-  if (led < 1 || led > 16) {
-    return -1;
-  }
-  if (leds_ == NULL) {
-    return -1;
-  }
+  RETURN_IF_INVALID_RANGE(led);
+
+  RETURN_IF_INVALID_ADDRESS(leds_);
 
   state_ |= NumberToBit(led);
   *leds_ = state_;
@@ -31,12 +38,8 @@ int Leds_TurnOn(int led) {
 };
 
 int Leds_TurnOff(int led) {
-  if (led < 1 || led > 16) {
-    return -1;
-  }
-  if (leds_ == NULL) {
-    return -1;
-  }
+  RETURN_IF_INVALID_RANGE(led);
+  RETURN_IF_INVALID_ADDRESS(leds_);
 
   state_ &= ~NumberToBit(led);
   *leds_ = state_;
@@ -44,10 +47,16 @@ int Leds_TurnOff(int led) {
 };
 
 int Led_TurnAllOn(void) {
-  if (leds_ == NULL) {
-    return -1;
-  }
+  RETURN_IF_INVALID_ADDRESS(leds_);
+
   state_ = ALL_LEDS_ON;
   *leds_ = state_;
   return 0;
+}
+
+int Leds_IsLedOn(int led) {
+  RETURN_IF_INVALID_RANGE(led);
+  RETURN_IF_INVALID_ADDRESS(leds_);
+
+  return state_ & NumberToBit(led);
 }
